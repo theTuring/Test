9e6d778ad73b039dafacf9bdba1694f627040d24
theTuring <46594931+theTuring@users.noreply.github.com>
2020-02-05 16:43:16 +0800

    Add files via upload

---
+++ 221701412/src/InfectStatistic.java
@@ -0,0 +1,13 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author 221701412_theTuring
+ * @version v 1.0.0
+ * @since 2020.2.4
+ */
+class InfectStatistic {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ 221701412/src/Lib.java
@@ -0,0 +1,10 @@
+/**
+ * Lib
+ * TODO
+ *
+ * @author 221701412_theTuring
+ * @version v 1.0.0
+ * @since 2020.2.4
+ */
+public class Lib {
+}
